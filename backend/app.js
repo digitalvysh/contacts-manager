ee15bdffc4f9fba6fd691ed9c04d5fcd51ad2040
digitalvysh <vyshnavibandla@gmail.com>
2022-06-01 15:01:12 +0530

    signinsingnupui

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const connectDB = require("./config/db");
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan("tiny"));
+
+app.get("/", (req, res) => {
+    res.send("hello world");
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, async() => {
+    try {
+        await connectDB();
+        console.log(`server listening on port: ${PORT}`);
+    } catch (err) {
+        console.log(err);
+    }
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import TotalContacts from "./pages/TotalContacts";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path = "/" element = {<Signin />} />
+        <Route path = "/signup" element = {<Signup />} />
+        <Route path = "/contact" element = {<TotalContacts />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
