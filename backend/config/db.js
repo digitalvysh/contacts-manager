2d31bd5e4c1096fb53d8ab52488fbe6a1a6ba432
ShehbazWaasi20 <shehbazwaasi@gmail.com>
2022-06-03 20:07:44 +0530

    frontend

--- backend/config/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = async() => {
-    return mongoose.connect("mongodb://localhost/contact_mern").then(() => console.log("connected to db..")).catch((err) => console.log(err));
-};
-
-module.exports = connectDB;
